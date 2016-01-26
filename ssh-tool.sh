b376d9e989fb1915976661f1732fdadd88e92ae6
Alastair <AKerr94@users.noreply.github.com>
2016-01-26 18:36:27 +0000

    attempts login-user detection for auto login

--- ssh-tool.sh
+++ ssh-tool.sh
@@ -3,20 +3,43 @@ YELLOW='\033[0;33m'
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m'
+
 openrc_location="openrc.sh" # Change this to the location of your downloaded OpenStack rc file
+
 function login {
-    echo "Enter user you would like to login as:"
-    read user
-    ssh $user@$(nova show $(nova list | grep -i $1 | awk '{ print $2 }') | grep network | awk '{ print $6 }')
+    # Attempt automatic login by querying for the OS (e.g. centos or ubuntu), else ask user for login user
+    server_n=$1
+    echo "server_name = $server_n"
+    server_id=$(nova list | grep -i $server_n | awk '{ print $2 }')
+    echo "server_id = $server_id"
+    server_ip=$(nova show $server_id | grep network | awk '{ print $6 }')
+    echo "server_ip = $server_ip"
+    nova show $server_id | grep image | grep -i 'centos'
+    if [ $? -eq 0 ];
+        then ssh centos@$server_ip
+    else
+        nova show $server_id | grep image | grep -i 'ubuntu'
+        if [ $? -eq 0 ];
+            then ssh ubuntu@$server_ip
+        else
+            echo "Enter user you would like to login as:"
+            read user
+            ssh $user@$server_ip
+        fi
+    fi
 }
 function login_msg {
     echo -e "Logging in to ${GREEN}$1${NC}..."
 }
+
 if [ -z "$OS_USERNAME" ];
-    then source $openrc_location; 
+    then source $openrc_location;
 fi
 echo "Enter OpenStack server name to search for:"
 read server_name
+if [ -z $server_name ];
+    then echo test
+fi
 server_list=($(nova list | grep -i $server_name | awk '{ print $4 }'))
 server_count=${#server_list[@]}
 if [ "$server_count" -gt 1 ];
@@ -32,7 +55,7 @@ if [ "$server_count" -gt 1 ];
     login_msg $chosen_server
     login $chosen_server
 elif [ "$server_count" -eq 1 ];
-    then login_msg $server_name
+    then login_msg $(nova list | grep -i $server_name | awk '{ print $4 }')
     login $server_name
 else
     echo -e "${RED}No matching servers were found.${NC}"
