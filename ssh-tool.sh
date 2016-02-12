7c0fb53d686d083181e2549cb64188d539de1fa6
Alastair <AKerr94@users.noreply.github.com>
2016-02-12 17:21:43 +0000

    Command line argument support for specifying region and server name

--- ssh-tool.sh
+++ ssh-tool.sh
@@ -4,10 +4,11 @@ GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m'
 
+# Configuration
 openrc_location="openrc.sh" # Change this to the location of your downloaded OpenStack rc file
 
 function login {
-    # Attempt automatic login by querying for the OS (e.g. centos or ubuntu), else ask user for login user
+    # Login with user from command line argument, else attempt automatic login by querying for the OS (e.g. centos or ubuntu), else ask user for login user
     server_n=$1
     server_id=$(nova list | grep -i $server_n | awk '{ print $2 }')
     echo "server_id = $server_id"
@@ -33,21 +34,56 @@ function login_msg {
     echo -e "Logging in to ${GREEN}$1${NC}..."
 }
 
+# Source openrc file if user hasn't done this already
 if [ -z "$OS_USERNAME" ];
     then source $openrc_location;
 fi
-echo -e "Current region: ${YELLOW}$OS_REGION_NAME${NC}"
-echo "Type region name to switch to or press enter if OK:"
-read region_name
-if ! [ -z $region_name ];
-    then export OS_REGION_NAME=$region_name
-fi
-echo -e "Using region ${YELLOW}$OS_REGION_NAME${NC}"
-echo "Enter OpenStack server name to search for:"
-read server_name
-if [ -z $server_name ];
-    then echo -e "${RED}Error: no server name specified${NC}"
+
+# Handle command line arguments
+REGION_SELECTED=false
+SERVER_SELECTED=false
+while [[ $# > 1 ]]
+do
+flag="$1"
+case $flag in
+    -r|--region)
+    OS_REGION_NAME="$2"
+    echo -e "Read in ${YELLOW}$OS_REGION_NAME${NC} as ${GREEN}OS_REGION_NAME${NC}"
+    REGION_SELECTED=true
+    shift
+    ;;
+    -s|--server)
+    server_name="$2"
+    echo -e "Read in ${YELLOW}$server_name${NC} as ${GREEN}server_name${NC}"
+    SERVER_SELECTED=true
+    shift
+    ;;
+    *)
+    # unknown option
+    echo -e "${RED}Unknown flag `echo $flag | tr -d '-'`!${NC}"
+    echo -e "${RED}Usage: Optional args -r <OS_REGION_NAME> -s <server_name>${NC}"
     exit 1
+esac
+shift
+done
+
+if [ "$REGION_SELECTED" = false ]; then
+    echo -e "Current region: ${YELLOW}$OS_REGION_NAME${NC}"
+    echo "Type region name to switch to or press enter if OK:"
+    read region_name
+    if ! [ -z $region_name ];
+        then export OS_REGION_NAME=$region_name
+    fi
+    echo -e "Using region ${YELLOW}$OS_REGION_NAME${NC}"
+fi
+
+if [ "$SERVER_SELECTED" = false ]; then
+    echo "Enter OpenStack server name to search for:"
+    read server_name
+    if [ -z $server_name ];
+        then echo -e "${RED}Error: no server name specified${NC}"
+        exit 1
+    fi
 fi
 server_list=($(nova list | awk '{if(NR>3)print $4}' | grep -i $server_name))
 server_count=${#server_list[@]}
