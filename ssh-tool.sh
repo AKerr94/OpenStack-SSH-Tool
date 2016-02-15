2dd6611eb577bd9d112f5379df1ab58c902951a4
Alastair <AKerr94@users.noreply.github.com>
2016-02-15 15:03:58 +0000

    Included support for -u flag
    Specify username to login to server as

--- ssh-tool.sh
+++ ssh-tool.sh
@@ -10,15 +10,26 @@ openrc_location="openrc.sh" # Change this to the location of your downloaded Ope
 function login {
     # Login with user from command line argument, else attempt automatic login by querying for the OS (e.g. centos or ubuntu), else ask user for login user
     server_n=$1
+    user=$2
+
+    # Query for server IP address to login to
     server_id=$(nova list | grep -i $server_n | awk '{ print $2 }')
     echo "server_id = $server_id"
     server_ip=$(nova show $server_id | grep network | awk '{ print $6 }')
     echo "server_ip = $server_ip"
+
+    # If user was supplied as command line argument, login as this user
+    if ! [ -z $user ] ; then
+        ssh $user@$server_ip
+        exit 0
+    fi
+
+    # Attempt OS detection to login as centos/ubuntu user automatically
     image_name=$(nova show $server_id | grep image | awk '{ print $5 }' | tr -d '()')
     image_desc=$(nova image-show $image_name | grep description)
     echo $image_desc | grep -qi 'centos'
-    if [ $? -eq 0 ] ;
-        then ssh centos@$server_ip
+    if [ $? -eq 0 ] ; then
+        ssh centos@$server_ip
     else
         echo $image_desc | grep -qi 'ubuntu'
         if [ $? -eq 0 ];
@@ -35,13 +46,14 @@ function login_msg {
 }
 
 # Source openrc file if user hasn't done this already
-if [ -z "$OS_USERNAME" ];
-    then source $openrc_location;
+if [ -z "$OS_USERNAME" ] ; then
+    source $openrc_location;
 fi
 
 # Handle command line arguments
 REGION_SELECTED=false
 SERVER_SELECTED=false
+USER_ARG=""
 while [[ $# > 1 ]]
 do
 flag="$1"
@@ -58,37 +70,42 @@ case $flag in
     SERVER_SELECTED=true
     shift
     ;;
+    -u|--user)
+    USER_ARG="$2"
+    echo -e "Read in ${YELLOW}$USER_ARG${NC} as ${GREEN}user${NC}"
+    shift
+    ;;
     *)
     # unknown option
     echo -e "${RED}Unknown flag `echo $flag | tr -d '-'`!${NC}"
-    echo -e "${RED}Usage: Optional args -r <OS_REGION_NAME> -s <server_name>${NC}"
+    echo -e "${RED}Usage: Optional args -r <OS_REGION_NAME> -s <server_name> -u <USERNAME> ${NC}"
     exit 1
 esac
 shift
 done
 
-if [ "$REGION_SELECTED" = false ]; then
+if [ "$REGION_SELECTED" = false ] ; then
     echo -e "Current region: ${YELLOW}$OS_REGION_NAME${NC}"
     echo "Type region name to switch to or press enter if OK:"
     read region_name
-    if ! [ -z $region_name ];
+    if ! [ -z $region_name ] ;
         then export OS_REGION_NAME=$region_name
     fi
     echo -e "Using region ${YELLOW}$OS_REGION_NAME${NC}"
 fi
 
-if [ "$SERVER_SELECTED" = false ]; then
+if [ "$SERVER_SELECTED" = false ] ; then
     echo "Enter OpenStack server name to search for:"
     read server_name
-    if [ -z $server_name ];
-        then echo -e "${RED}Error: no server name specified${NC}"
+    if [ -z $server_name ] ; then
+        echo -e "${RED}Error: no server name specified${NC}"
         exit 1
     fi
 fi
 server_list=($(nova list | awk '{if(NR>3)print $4}' | grep -i $server_name))
 server_count=${#server_list[@]}
-if [ "$server_count" -gt 1 ];
-    then echo -e "Found ${YELLOW}$server_count${NC} matching servers..."
+if [ "$server_count" -gt 1 ] ; then
+    echo -e "Found ${YELLOW}$server_count${NC} matching servers..."
     count=1
     for server in ${server_list[@]};
         do echo -e "[${YELLOW}$count${NC}] $server"
@@ -98,10 +115,10 @@ if [ "$server_count" -gt 1 ];
     read server_number
     chosen_server=${server_list[$(($server_number -1))]}
     login_msg $chosen_server
-    login $chosen_server
-elif [ "$server_count" -eq 1 ];
-    then login_msg ${server_list[0]}
-    login ${server_list[0]}
+    login $chosen_server $USER_ARG
+elif [ "$server_count" -eq 1 ] ; then
+    login_msg ${server_list[0]}
+    login ${server_list[0]} $USER_ARG
 else
     echo -e "${RED}No matching servers were found.${NC}"
 fi
